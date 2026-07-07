df38f190153bef2a39a7b5d665fa8be5a8d282ca
fretelli <fretelli@users.noreply.github.com>
2026-07-07 23:14:03 +0800

    refactor(web): split research hub modules

---
+++ keeltrader/apps/web/lib/research/index.ts
@@ -0,0 +1,11 @@
+"use client";
+
+export * from "./types";
+export * from "./client";
+export * from "./attribution";
+export * from "./reports";
+export * from "./profile";
+export * from "./billing";
+export * from "./funds";
+export * from "./feedback";
+export * from "./files";
